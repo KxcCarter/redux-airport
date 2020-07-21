4edabf102ad24efe345cb91907d6a716c0211903
Kenneth Carter <kxccarter@gmail.com>
2020-07-21 14:45:14 -0500

    Adding ans displaying an airline seems to work

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,50 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class App extends Component {
+  // Local state set up to temporarily store value from input.
+  state = {
+    enteredAirline: '',
+  };
+
+  // The handleChange method is the same as before Redux.
+  // It just deals with the local state.
+  handleInputChange = (event) => {
+    this.setState({
+      enteredAirline: event.target.value,
+    });
+  };
+
+  clickAddAirline = () => {
+    this.props.dispatch({
+      type: 'ADD_AIRLINE',
+      payload: this.state.enteredAirline,
+    });
+  };
+
   render() {
+    const airlinesInAirport = this.props.store.airlineList.map((item, index) => {
+      return (
+        <tr key={index}>
+          <td>{item}</td>
+        </tr>
+      );
+    });
     return (
       <div>
         <h1>Redux Airport</h1>
-        <input placeholder="Airline Name" />
-        <button>Add Airline</button>
-        <table>
-          {/* Airlines should be listed here */}
-        </table>
+        <input onChange={this.handleInputChange} placeholder="Airline Name" />
+        <button onClick={this.clickAddAirline}>Add Airline</button>
+        <table>{airlinesInAirport}</table>
       </div>
     );
   }
 }
 
-export default App;
+const mapStoreToProps = (store) => {
+  return {
+    store,
+  };
+};
+
+export default connect(mapStoreToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,5 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// This is a reducer. It handles state for arrays or objects.
+const airlineList = (state = ['Luftansa'], action) => {
+  if (action.type === 'ADD_AIRLINE') {
+    // I'm only sending a string on payload, so I don't need to add
+    // anything like '.data' or '.myAddedStuff'
+    return [...state, action.payload];
+  }
+
+  // state currently is an array that holds 'Luftansa'
+  // A reducer always has to return something.
+  return state;
+};
+
+// This can be called anything.
+// It takes all the reducers as arguments.
+const storedInformation = createStore(
+  combineReducers({
+    airlineList,
+  })
+);
+
+// Wrap App in Provider to give App access to state stored in index.js
+ReactDOM.render(
+  <Provider store={storedInformation}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
